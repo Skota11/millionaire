ca818728d6672cf3e4b9c3220fdcf37bfc1acb8d
S Kota <91359399+Skota11@users.noreply.github.com>
2024-03-15 11:45:25 +0900

    Fix:LinkTag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
         <main>
             <h1 className="text-center text-2xl py-8">オンライン大富豪</h1>
             <div className="flex place-content-center">
-                <a className="border-solid border-2 p-4 text-center" href="/play">プレイする</a>
+                <Link className="border-solid border-2 p-4 text-center" href="/play">プレイする</Link>
             </div>
         </main>
     );
